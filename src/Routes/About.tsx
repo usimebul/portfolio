423fd9cfee51fc3f3616a26dbb2654f94102d9d2
usimebul <45548024+usimebul@users.noreply.github.com>
2022-04-19 15:08:02 +0900

    initial commit

--- src/Routes/About.tsx
+++ src/Routes/About.tsx
@@ -1,50 +1,50 @@
-import {Content, P, TextWrapper} from "../Components/UI";
+import { Content, P, TextWrapper, Text } from "../Components/UI";
 import Character from "../Components/Character";
-import {css} from "@emotion/react";
-import styled from "@emotion/styled";
-
-const Text = styled.span`
-  display: block;
-  margin-block-start: 1em;
-  margin-block-end: 1em;
-  margin-inline-start: 0;
-  margin-inline-end: 0;
-`
+import { css } from "@emotion/react";
 
 function About() {
-    return (
-        <Content>
-            <TextWrapper>
-                <Character custom={0} content={"M"}/>
-                <Character custom={1} content={"y"}/>
-                <Character custom={2} content={",\u00A0"}/>
-                <Character custom={3} content={"M"}/>
-                <Character custom={4} content={"y"}/>
-                <Character custom={5} content={"s"}/>
-                <Character custom={6} content={"e"}/>
-                <Character custom={7} content={"l"}/>
-                <Character custom={8} content={"f\u00A0"}/>
-                <Character custom={9} content={"&\u00A0"}/>
-                <Character custom={10} content={"I"}/>
-            </TextWrapper>
+  return (
+    <Content>
+      <TextWrapper>
+        <Character custom={0} content={"M"} />
+        <Character custom={1} content={"y"} />
+        <Character custom={2} content={",\u00A0"} />
+        <Character custom={3} content={"M"} />
+        <Character custom={4} content={"y"} />
+        <Character custom={5} content={"s"} />
+        <Character custom={6} content={"e"} />
+        <Character custom={7} content={"l"} />
+        <Character custom={8} content={"f\u00A0"} />
+        <Character custom={9} content={"&\u00A0"} />
+        <Character custom={10} content={"I"} />
+      </TextWrapper>
 
-            <P css={css`
-              color: white;
-              width: 50%;
-              min-width: 37em`
-            }>
-                <Text>I'm a Web Front-End Developer has experiences to develop cryptographic module, PKI solutions,
-                    and user authentication services. I'm aiming to be Front-End centric Full-Stack Developer and
-                    expanding my tech stack to server-side works (server, database, and infrastructure)</Text>
-                <Text>I'm a Logician (INTP), problem solver, challenger, and positive person. I like to watch TV shows,
-                    read genre literature, sing a song. I love the song of IU, Akdong Musician, Maktub, and Jeong
-                    Seung-hwan. I work out more than three times a week, but I don't like workout.
-                </Text>
-                <Text>I'm interested in UI design, effects, animations and creating intuitive, dynamic user
-                    experiences. </Text>
-            </P>
-        </Content>
-    );
+      <P
+        css={css`
+          color: white;
+          width: 50%;
+          min-width: 37em;
+        `}
+      >
+        <Text>
+          I'm a Web Front-End Developer has experiences to develop cryptographic
+          module, PKI solutions, and user authentication services. I'm aiming to
+          be Front-End centric Full-Stack Developer and expanding my tech stack
+          to server-side works (server, database, and infrastructure)
+        </Text>
+        <Text>
+          I'm a Logician (INTP), problem solver, challenger, and positive
+          person. I like to watch TV shows, read genre literature, sing a song.
+          I love the song of IU, Akdong Musician, Maktub, and Jeong Seung-hwan.
+          I work out more than three times a week, but I don't like workout.
+        </Text>
+        <Text>
+          I'm interested in UI design, effects, animations and creating
+          intuitive, dynamic user experiences.{" "}
+        </Text>
+      </P>
+    </Content>
+  );
 }
 
 export default About;
