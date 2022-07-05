aa37b1a9d6c0bcd33b82e0f5732857a07b51f022
khan <usimebul@gmail.com>
2022-07-05 18:20:39 +0900

    220705 재개

--- src/Routes/About.tsx
+++ src/Routes/About.tsx
@@ -1,110 +1,50 @@
+import { Content, P, TextWrapper, Text } from "../Components/UI";
 import Character from "../Components/Character";
-import {css} from "@emotion/react";
-import {Content, Section, TextWrapper} from "../StyledComponents/Layout";
-import {P, Span} from "../StyledComponents/Component";
-
-import * as THREE from 'three'
-import React, {Suspense, useRef, useState} from 'react'
-import {Canvas, useFrame, useLoader} from '@react-three/fiber'
-import {FBXLoader} from "three/examples/jsm/loaders/FBXLoader";
-import {Html, useProgress, OrbitControls} from '@react-three/drei'
-
-function Loader() {
-    const {progress} = useProgress()
-    return <Html center>{progress} % loaded</Html>
-}
-
-function Scene() {
-    const object = useLoader(FBXLoader, "hello.fbx");
-    let mixer = new THREE.AnimationMixer(object)
-    let clips = object.animations;
-    clips.forEach( function ( clip ) {
-        mixer.clipAction( clip ).play();
-    } );
-
-    return <primitive object={object} scale={0.02} position={{x:0, y:0, z:0}}/>;
-}
-
-function MyAnimatedBox() {
-    const myMesh = React.useRef<THREE.Mesh>()
-    useFrame(({ clock }) => {
-        myMesh.current!.rotation.x = clock.getElapsedTime()
-    })
-    return (
-        // @ts-ignore
-        <mesh ref={myMesh}>
-            <boxGeometry/>
-            <meshBasicMaterial color="royalblue"/>
-        </mesh>
-    )
-}
-
-
-function Box(props: JSX.IntrinsicElements['mesh']) {
-    const ref = useRef<THREE.Mesh>(null!)
-    const [hovered, hover] = useState(false)
-    const [clicked, click] = useState(false)
-    useFrame((state, delta) => (ref.current.rotation.x += 0.01))
-    return (
-        <mesh
-            {...props}
-            ref={ref}
-            scale={clicked ? 1.5 : 1}
-            onClick={(event) => click(!clicked)}
-            onPointerOver={(event) => hover(true)}
-            onPointerOut={(event) => hover(false)}>
-            <boxGeometry args={[1, 1, 1]}/>
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'}/>
-        </mesh>
-    )
-}
-
+import { css } from "@emotion/react";
 
 function About() {
-    return (
-        <Content>
-            <Section>
-                <TextWrapper>
-                    <Character custom={0} content={"M"}/>
-                    <Character custom={1} content={"y"}/>
-                    <Character custom={2} content={",\u00A0"}/>
-                    <Character custom={3} content={"M"}/>
-                    <Character custom={4} content={"y"}/>
-                    <Character custom={5} content={"s"}/>
-                    <Character custom={6} content={"e"}/>
-                    <Character custom={7} content={"l"}/>
-                    <Character custom={8} content={"f\u00A0"}/>
-                    <Character custom={9} content={"&\u00A0"}/>
-                    <Character custom={10} content={"I"}/>
-                </TextWrapper>
+  return (
+    <Content>
+      <TextWrapper>
+        <Character custom={0} content={"M"} />
+        <Character custom={1} content={"y"} />
+        <Character custom={2} content={",\u00A0"} />
+        <Character custom={3} content={"M"} />
+        <Character custom={4} content={"y"} />
+        <Character custom={5} content={"s"} />
+        <Character custom={6} content={"e"} />
+        <Character custom={7} content={"l"} />
+        <Character custom={8} content={"f\u00A0"} />
+        <Character custom={9} content={"&\u00A0"} />
+        <Character custom={10} content={"I"} />
+      </TextWrapper>
 
-                <P css={css`
-                  color: white;
-                  width: 50%;
-                  min-width: 37em`
-                }>
-                    <Span>I'm a Web Front-End Developer has experiences to develop cryptographic module, PKI solutions,
-                        and user authentication services. I'm aiming to be Front-End centric Full-Stack Developer and
-                        expanding my tech stack to server-side works (server, database, and infrastructure)</Span>
-                    <Span>I'm a Logician (INTP), problem solver, challenger, and positive person. I like to watch TV
-                        shows,
-                        read genre literature, sing a song. I love the song of IU, Akdong Musician, Maktub, and Jeong
-                        Seung-hwan. I work out more than three times a week, but I don't like workout.
-                    </Span>
-                    <Span>I'm interested in UI design, effects, animations and creating intuitive, dynamic user
-                        experiences. </Span>
-                </P>
-            </Section>
-            <Section>
-                <Canvas>
-                    <Suspense fallback={<Loader/>}>
-                        <Scene/>
-                        <OrbitControls/>
-                    </Suspense>
-                </Canvas>
-            </Section>
-        </Content>
-    );
+      <P
+        css={css`
+          color: white;
+          width: 40%;
+          min-width: 37em;
+        `}
+      >
+        <Text>
+          I'm a Web Front-End Developer has experiences to develop cryptographic
+          module, PKI solutions, and user authentication services. I'm aiming to
+          be Front-End centric Full-Stack Developer and expanding my tech stack
+          to server-side works (server, database, and infrastructure)
+        </Text>
+        <Text>
+          I'm a Logician (INTP), problem solver, challenger, and positive
+          person. I like to watch TV shows, read genre literature, sing a song.
+          I love the song of IU, Akdong Musician, Maktub, and Jeong Seung-hwan.
+          I work out more than three times a week, but I don't like workout.
+        </Text>
+        <Text>
+          I'm interested in UI design, effects, animations and creating
+          intuitive, dynamic user experiences.{" "}
+        </Text>
+      </P>
+    </Content>
+  );
 }
 
 export default About;
--- src/Routes/Skills.tsx
+++ src/Routes/Skills.tsx
@@ -1,66 +1,72 @@
-import { Content, P, TextWrapper, Text } from "../Components/UI";
+import {Content, P, TextWrapper, Text} from "../Components/UI";
 import Character from "../Components/Character";
-import { css } from "@emotion/react";
+import {css} from "@emotion/react";
 
 function Skills() {
-  return (
-    <Content>
-      <TextWrapper>
-        <Character custom={0} content={"S"} />
-        <Character custom={1} content={"k"} />
-        <Character custom={2} content={"i"} />
-        <Character custom={3} content={"l"} />
-        <Character custom={4} content={"l"} />
-        <Character custom={5} content={"s\u00A0"} />
-        <Character custom={6} content={"&"} />
-        <br />
-        <Character custom={10} content={"E"} />
-        <Character custom={11} content={"x"} />
-        <Character custom={12} content={"p"} />
-        <Character custom={13} content={"e"} />
-        <Character custom={14} content={"r"} />
-        <Character custom={15} content={"i"} />
-        <Character custom={16} content={"e"} />
-        <Character custom={17} content={"c"} />
-        <Character custom={18} content={"e"} />
-      </TextWrapper>
+    return (
+        <Content>
+            <TextWrapper>
+                <Character custom={0} content={"S"}/>
+                <Character custom={1} content={"k"}/>
+                <Character custom={2} content={"i"}/>
+                <Character custom={3} content={"l"}/>
+                <Character custom={4} content={"l"}/>
+                <Character custom={5} content={"s\u00A0"}/>
+                <Character custom={6} content={"&"}/>
+                <br/>
+                <Character custom={10} content={"E"}/>
+                <Character custom={11} content={"x"}/>
+                <Character custom={12} content={"p"}/>
+                <Character custom={13} content={"e"}/>
+                <Character custom={14} content={"r"}/>
+                <Character custom={15} content={"i"}/>
+                <Character custom={16} content={"e"}/>
+                <Character custom={17} content={"c"}/>
+                <Character custom={18} content={"e"}/>
+            </TextWrapper>
+            <P
+                css={css`
+                  color: white;
+                  width: 40%;
+                  min-width: 37em;
+                `}
+            >
+                <Text>
+                    I've worked maintaining legacy PKI solutions. The first implementation
+                    on this company is the browser-based PKI solution for providing
+                    functionalities without a program installation. From this exprience, I
+                    learned about the basics of HTML, CSS, Javascript, Browsers, Web APIs,
+                    and Web Securitiy.
+                </Text>
+                <Text>
+                    Also I implemented the service which intermediates among the app based
+                    user authentication services such as Samsung Pass, (KT, SKT, LGT)
+                    PASS, Naver Certifiate, Kakao Certificate, and Payco Certificate. And
+                    I intergrated our authentication solutions (PKI solutions and app
+                    based user authentication). From this exprience, I learned about
+                    back-end works such as internet (HTTP, DNS, hosting, Firewall, Closed
+                    Network, and etc.), frameworks and libariens (Spring Boot, Webpack,
+                    and Babel), and servers (Apaceh, Tomcat, and JBoss EAP).
+                </Text>
+                <Text>
+                    Now what I do is implements services (Mobile ID (mobile driver
+                    license), MYDATA, and Cloud Certificates) and web pages
+                    (administration) which is applied React and maintains solutions.
+                </Text>
+                <Text>
+                    I'm skillfull in Front-End development and engineering and have basic
+                    of Back-End works and the near-professional PKI knowledge.
+                </Text>
+            </P>
 
-      <P
-        css={css`
-          color: white;
-          width: 50%;
-          min-width: 37em;
-        `}
-      >
-        <Text>
-          I've worked maintaining legacy PKI solutions. The first implementation
-          on this company is the browser-based PKI solution for providing
-          functionalities without a program installation. From this exprience, I
-          learned about the basics of HTML, CSS, Javascript, Browsers, Web APIs,
-          and Web Securitiy.
-        </Text>
-        <Text>
-          Also I implemented the service which intermediates among the app based
-          user authentication services such as Samsung Pass, (KT, SKT, LGT)
-          PASS, Naver Certifiate, Kakao Certificate, and Payco Certificate. And
-          I intergrated our authentication solutions (PKI solutions and app
-          based user authentication). From this exprience, I learned about
-          back-end works such as internet (HTTP, DNS, hosting, Firewall, Closed
-          Network, and etc.), frameworks and libariens (Spring Boot, Webpack,
-          and Babel), and servers (Apaceh, Tomcat, and JBoss EAP).
-        </Text>
-        <Text>
-          Now what I do is implements services (Mobile ID (mobile driver
-          license), MYDATA, and Cloud Certificates) and web pages
-          (administration) which is applied React and maintains solutions.
-        </Text>
-        <Text>
-          I'm skillfull in Front-End development and engineering and have basic
-          of Back-End works and the near-professional PKI knowledge.
-        </Text>
-      </P>
-    </Content>
-  );
+            <div css={css`{
+              background-color: white;
+              width: 100%;
+              height: 100%;
+              position: fixed;
+            }`}></div>
+        </Content>
+    );
 }
 
 export default Skills;
