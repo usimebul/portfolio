f3e5ffa2204ff49727d71b17fe14fe242f299605
khan <usimebul@gmail.com>
2022-07-25 18:24:12 +0900

    스킬 아이템 업데이트

--- src/Routes/Skills.tsx
+++ src/Routes/Skills.tsx
@@ -2,6 +2,55 @@ import {P, Text} from "../StyledComponents/Component";
 import {Content, TextWrapper, Section} from "../StyledComponents/Layout";
 import Character from "../Components/Character";
 import {css} from "@emotion/react";
+import styled from "@emotion/styled";
+import {motion} from "framer-motion";
+
+const SkillSection = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+
+const SkillSectionHeader = styled.div`
+  font-size: 5.6rem;
+  font-weight: 500;
+  color: ${(props) => props.theme.textColor.highlight};
+  font-family: font-file-82132, sans-serif;
+  display: block;
+`
+
+const SkillSectionBody = styled.div`
+
+`
+
+const SkillItem = styled.div`
+  margin-top: 14px;
+`
+
+const SkillItemHeader = styled.div`
+  font-size: 2.6rem;
+  font-weight: 500;
+  color: ${(props) => props.theme.textColor.content};
+  margin-bottom: 7px;
+  display: block;
+`
+
+const SkillItemDetail = styled.div`
+  font-size: 1.6rem;
+  color: #8d8d8d;
+`
+
+const LevelIndicatorWrapper = styled.div`
+  margin-top: 1em;
+  background-color: #373737;
+  height: 2px;
+`
+const LevelIndicator = styled(motion.div)`
+  width: 0;
+  height: 100%;
+  background-color: #08fdd8;
+`;
 
 function Skills() {
     return (
@@ -34,26 +83,31 @@ function Skills() {
                     `}
                 >
                     <Text>
+                        I've developed PKI related solutions and services.
+                        The PKI solutions are used at webpages such as login and authorization.
+                        To meet various client environment (OS and browsers), I expertise web engineering.
+
+
                         I've worked maintaining legacy PKI solutions. The first implementation
                         on this company is the browser-based PKI solution for providing
-                        functionalities without a program installation. From this exprience, I
+                        functionalities without a program installation. From this experience, I
                         learned about the basics of HTML, CSS, Javascript, Browsers, Web APIs,
-                        and Web Securitiy.
+                        and Web Security.
                     </Text>
                     <Text>
                         Also I implemented the service which intermediates among the app based
                         user authentication services such as Samsung Pass, (KT, SKT, LGT)
-                        PASS, Naver Certifiate, Kakao Certificate, and Payco Certificate. And
-                        I intergrated our authentication solutions (PKI solutions and app
-                        based user authentication). From this exprience, I learned about
+                        PASS, Naver Certificate, Kakao Certificate, and Payco Certificate. And
+                        I integrated our authentication solutions (PKI solutions and app
+                        based user authentication). From this experience, I learned about
                         back-end works such as internet (HTTP, DNS, hosting, Firewall, Closed
-                        Network, and etc.), frameworks and libariens (Spring Boot, Webpack,
+                        Network, and etc.), frameworks and librarians (Spring Boot, Webpack,
                         and Babel), and servers (Apaceh, Tomcat, and JBoss EAP).
                     </Text>
                     <Text>
                         Now what I do is implements services (Mobile ID (mobile driver
                         license), MYDATA, and Cloud Certificates) and web pages
-                        (administration) which is applied React and maintains solutions.
+                        (administration) which is applied React.
                     </Text>
                     <Text>
                         I'm skillfull in Front-End development and engineering and have basic
@@ -61,27 +115,46 @@ function Skills() {
                     </Text>
                 </P>
             </Section>
-            <div>
-                <div>Front-end</div>
-                <div>
-                    <div>
-                        <div>Basics</div>
-                        <div>HTML, Javascript, CSS, Browsers, Web Accessibility, Web Security
-                        </div>
-                        <div>High</div>
-                    </div>
-                    <div>
-                        <div>Framework and Libraries</div>
-                        <div>React, React Native, Next.js, Styled Component, Emotion, Framer Motion, Threejs,
-                            Babel, Webpack, Javascript Obfausctsor, forge
-                        </div>
-                        <div>Medium</div>
-                    </div>
-                </div>
-            </div>
-            <Section/>
-
-
+            <Section>
+                <SkillSection>
+                    <SkillSectionHeader>Front-end</SkillSectionHeader>
+                    <SkillSectionBody>
+                        <SkillItem>
+                            <SkillItemHeader>Basics</SkillItemHeader>
+                            <SkillItemDetail>Internet and Browsers Mechanism, HTML(Semantic HTML, SEO,
+                                Accessibility, Web Components), Javascript(Fetch API/Ajax, DOM manipulation, Event
+                                Handling), CSS(Layouts(display, position, flex, grid), Animation, Responsive UI)
+                            </SkillItemDetail>
+                            <LevelIndicatorWrapper>
+                                <LevelIndicator
+                                    animate={{
+                                        width: "90%"
+                                    }}
+                                    transition={{
+                                        duration: 0.6
+                                    }}/>
+                            </LevelIndicatorWrapper>
+                        </SkillItem>
+                        <SkillItem>
+                            <SkillItemHeader>Frameworks and Libraries</SkillItemHeader>
+                            <SkillItemDetail>React, React Native, Next.js, Styled Component, Emotion, Framer Motion,
+                                Threejs,
+                                Babel, Webpack, Javascript Obfausctsor, forge.js
+                            </SkillItemDetail>
+                            <LevelIndicatorWrapper>
+                                <LevelIndicator
+                                    animate={{
+                                        width: "60%"
+                                    }}
+                                    transition={{
+                                        duration: 0.6
+                                    }}/>
+                            </LevelIndicatorWrapper>
+                        </SkillItem>
+                    </SkillSectionBody>
+                </SkillSection>
+                <SkillSection></SkillSection>
+            </Section>
         </Content>
     );
 }
